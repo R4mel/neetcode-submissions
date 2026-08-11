9a644bfbc8b4371d3958cc4fa509c083e6a510d5
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-11 23:34:12 +0900

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        if (n != m) {
+            return false;
+        }
+        HashMap<Character, Integer> ss = new HashMap<>(n);
+        HashMap<Character, Integer> tt = new HashMap<>(m);
+
+        for (int i = 0; i < n; i++) {
+            ss.put(s.charAt(i), ss.getOrDefault(s.charAt(i), 0) + 1);
+            tt.put(t.charAt(i), tt.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        for (Character c : ss.keySet()) {
+            if (!ss.get(c).equals(tt.getOrDefault(c, 0))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
